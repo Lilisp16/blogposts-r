8f0383474225e8887db01de89bd8f96fb59197c0
Lilisp16 <lijhosa@gmail.com>
2025-05-09 11:29:41 -0500

    Actualizar Procfile y run.py para configurar timeout

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os
+
 from blogr import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+ 
+
+ # Usa el puerto proporcionado por Render y asegura que el host esté configurado correctamente
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
